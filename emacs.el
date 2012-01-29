301fbef3680de4d463fe40ddc2b852bdcccce412
Julson Lim <julsonlim@gmail.com>
2012-01-29 00:23:37 -0800

    Removed elpa initialization code, since this is already included in Emacs 24

--- emacs.el
+++ emacs.el
@@ -78,12 +78,3 @@
  nxml-degraded t)
 (add-to-list 'auto-mode-alist '("\\.html\\.erb'" . eruby-nxhtml-mumamo))
 
-;;; This was installed by package-install.el.
-;;; This provides support for the package system and
-;;; interfacing with ELPA, the package archive.
-;;; Move this code earlier if you want to reference
-;;; packages in your .emacs.
-(when
-    (load
-     (expand-file-name "~/.emacs.d/elpa/package.el"))
-  (package-initialize))

31d41e7be1d30d5ebf618c873253ad9843b94db4
Julson Lim <julsonlim@gmail.com>
2012-09-12 14:54:50 -0700

    Forgot to add elscreen as part of the package check

--- emacs.el
+++ emacs.el
@@ -13,7 +13,7 @@
 (defvar personal-packages
   '(clojure-mode magit magithub org paredit nrepl color-theme-solarized yasnippet
                  markdown-mode melpa ido-ubiquitous clojurescript-mode crontab-mode
-                 multi-term )
+                 multi-term elscreen)
   "A list of packages to ensure are installed at launch.")
 
 (defun personal-packages-installed-p ()
@@ -115,3 +115,15 @@
 (add-hook 'scheme-mode-hook (lambda () (paredit-mode +1)))
 (add-hook 'clojure-mode-hook (lambda () (paredit-mode +1)))
 (add-hook 'slime-repl-mode-hook (lambda () (paredit-mode +1)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
